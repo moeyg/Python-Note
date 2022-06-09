dc1b301b77b03910505b9fa91094eb62081afeab
moeyg <moooeyg@gmail.com>
2022-06-09 13:37:26 +0900

    Add Datatype

---
+++ 02.datatype/2-01.data-type.py
@@ -0,0 +1,22 @@
+# 숫자 자료형 (Int)
+print(10)  # 10
+print(-10)  # -10
+print(3.14)  # 3.14
+print(1000)  # 1000
+print(5 + 2)  # 7
+print(5 * 2)  # 10
+print(5 * (5 + 2))  # 35
+
+# 문자 자료형 (String)
+print('Hello World!')  # Hello World!
+print("Hello World!")  # Hello World!
+print("Hello World! " * 3)  # Hello World! Hello World! Hello World!
+
+# 참 / 거짓 자료형 (Boolean)
+print(1 > 10)  # False
+print(1 < 10)  # True
+print(True)  # True
+print(False)  # False
+print(not True)  # False
+print(not False)  # True
+print(not (1 > 10))  # True
---
+++ 02.datatype/2-02.variable.py
@@ -0,0 +1,12 @@
+# 변수 (Variable)
+
+# 반려동물 소개하기
+animal = "고양이"
+name = "파도"
+age = 2
+hobby = "낚시 놀이"
+isAdult = age >= 3
+
+print("우리 집 "+animal+"의 이름은 "+name+" 입니다.")
+print(""+name+"는 "+str(age)+"살이고, "+hobby+"를 아주 좋아합니다.")
+print(""+name+"는 어른 "+animal+"일까요? "+str(isAdult)+"")
